38cf98566cf53e94167c3ab3896a828254ace8b7
Hamad <AlmarzooqiH23@gmail.com>
2025-01-09 18:49:04 +0400

    Adding the foundation of the program.

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -6,11 +6,55 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 12:20:04 by hamad             #+#    #+#             */
-/*   Updated: 2025/01/09 12:22:12 by hamad            ###   ########.fr       */
+/*   Updated: 2025/01/09 13:58:53 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
+# include <string.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+/**
+ * @brief Structure to hold the information of the philosophers.
+ * @var id The id of the philosopher.
+ * @var np The number of philosophers.
+ * @var td The time to die.
+ * @var te The time to eat.
+ * @var mstf milliseconds when he took the fork.
+ * @var mse milliseconds when he started eating.
+ * @var mss milliseconds when he started sleeping.
+ * @var mst milliseconds when he started thinking.
+ * @var msd milliseconds when he died.
+ */
+typedef struct s_philo
+{
+	int		id;
+	float	td;
+	float	te;
+	float	mstf;
+	float	mse;
+	float	mss;
+	float	mst;
+	float	msd;
+}	t_philo;
+
+/**
+ * @brief Structure to hold the information of the program.
+ * @var n_philo The number of philosophers.
+ * @var neat The number of eatings. (optional)
+ * @var time The time when the simulation started/ended.
+ * @var philo The array of philosophers.
+ */
+typedef struct s_prog
+{
+	int		n_philo;
+	int		neat;
+	float	time;
+	t_philo	*philo;
+}	t_prog;
 #endif
