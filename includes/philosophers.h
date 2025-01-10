3661a1c2c9ff0f50c42eb7510d0494e423dfa73d
Hamad <AlmarzooqiH23@gmail.com>
2025-01-10 21:08:55 +0400

    Modified the structs and add funtion prototypes.

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 12:20:04 by hamad             #+#    #+#             */
-/*   Updated: 2025/01/09 13:58:53 by hamad            ###   ########.fr       */
+/*   Updated: 2025/01/10 20:51:03 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,8 @@
 /**
  * @brief Structure to hold the information of the philosophers.
  * @var id The id of the philosopher.
- * @var np The number of philosophers.
- * @var td The time to die.
- * @var te The time to eat.
+ * @var lh The status of the left hand. (If it has the fork).
+ * @var rh The status of the right hand. (If it has the fork).
  * @var mstf milliseconds when he took the fork.
  * @var mse milliseconds when he started eating.
  * @var mss milliseconds when he started sleeping.
@@ -34,18 +33,21 @@
 typedef struct s_philo
 {
 	int		id;
-	float	td;
-	float	te;
-	float	mstf;
-	float	mse;
-	float	mss;
-	float	mst;
-	float	msd;
+	int		lh;
+	int		rh;
+	long	mstf;
+	long	mse;
+	long	mss;
+	long	mst;
+	long	msd;
 }	t_philo;
 
 /**
  * @brief Structure to hold the information of the program.
  * @var n_philo The number of philosophers.
+ * @var	td The time to die (time in milliseconds).
+ * @var te The time to eat (time in milliseconds).
+ * @var ts The time to sleep (time in milliseconds).
  * @var neat The number of eatings. (optional)
  * @var time The time when the simulation started/ended.
  * @var philo The array of philosophers.
@@ -53,8 +55,13 @@ typedef struct s_philo
 typedef struct s_prog
 {
 	int		n_philo;
+	long	time;
+	long	td;
+	long	te;
+	long	ts;
 	int		neat;
-	float	time;
 	t_philo	*philo;
 }	t_prog;
+
+long	ft_atol(const char *str);
 #endif
