468167e2e9c42acf06d6c6bdb67fdf11baf50617
Hamad <AlmarzooqiH23@gmail.com>
2025-01-11 16:25:50 +0400

    Added some helper function.

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 20:49:35 by hamad             #+#    #+#             */
-/*   Updated: 2025/01/10 20:50:44 by hamad            ###   ########.fr       */
+/*   Updated: 2025/01/11 15:37:42 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ long	ft_atol(const char *str)
 	int			sign;
 	long long	num;
 
+	if (!str)
+		return (0);
 	sign = 1;
 	num = 0;
 	while ((*str >= 9 && *str <= 13) || *str == 32)
@@ -36,4 +38,36 @@ long	ft_atol(const char *str)
 	if (sign == -1)
 		num = -num;
 	return (num);
+}
+
+int	ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int	ft_isspace(int c)
+{
+	return ((c >= 9 && c <= 13) || c == 32);
+}
+
+int	ft_isonlyspace(const char *s)
+{
+	size_t	i;
+
+	if (!s || s[0] == '\0')
+		return (0);
+	i = 0;
+	while (ft_isspace(s[i]))
+		i++;
+	return (i == ft_strlen(s));
 }
