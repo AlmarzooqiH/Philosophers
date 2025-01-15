b230358eb69adeae462d62da24a6482b8047aca0
Hamad <AlmarzooqiH23@gmail.com>
2025-01-15 06:10:45 +0400

    Added ft_bzero() and ft_calloc().

---
+++ utils2.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 05:55:48 by hamad             #+#    #+#             */
+/*   Updated: 2025/01/15 05:57:50 by hamad            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/philosophers.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	if (n <= 0)
+		return ;
+	i = 0;
+	while (i < n)
+	{
+		*(unsigned char *)(s + i) = 0;
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*intialize;
+
+	intialize = malloc(count * size);
+	if (intialize == NULL)
+		return (NULL);
+	ft_bzero(intialize, size * count);
+	return (intialize);
+}
