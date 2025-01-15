ca74698d3e65b6b45a666ab45baeaf0521b5b0db
Hamad <AlmarzooqiH23@gmail.com>
2025-01-15 07:42:15 +0400

    Added the function that will start and join the philosphers threads. (It does not have a function yet so for the function i passed NULL for now.)

--- main.c
+++ main.c
@@ -6,11 +6,32 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 12:20:35 by hamad             #+#    #+#             */
-/*   Updated: 2025/01/15 06:09:16 by hamad            ###   ########.fr       */
+/*   Updated: 2025/01/15 07:40:53 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/philosophers.h"
+
+int	start_threads(t_prog *prog)
+{
+	int	i;
+
+	i = 0;
+	while (i < prog->n_philo)
+	{
+		if (pthread_create(&prog->philo[i].tid, NULL, NULL, &prog->philo[i]))
+			return (0);
+		i++;
+	}
+	i = 0;
+	while (i < prog->n_philo)
+	{
+		if (pthread_join(prog->philo[i].tid, NULL))
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 int	main(int ac, char **av)
 {
@@ -23,6 +44,8 @@ int	main(int ac, char **av)
 		return (printf("Failed to calloc t_prog\n"), EXIT_FAILURE);
 	if (init(ac, av, prog))
 		return (EXIT_FAILURE);
+	if (!start_threads(prog))
+		return (printf("%s", FTT), free_prog(prog), EXIT_FAILURE);
 	free_prog(prog);
 	return (EXIT_SUCCESS);
 }
