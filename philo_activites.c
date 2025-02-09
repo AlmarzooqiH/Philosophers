83d56284142bdfbe34975afeaf5f2df84136afbd
Hamad <AlmarzooqiH23@gmail.com>
2025-02-10 02:30:39 +0400

    Added the function that will print which activity was doing by the philosopher.

--- philo_activites.c
+++ philo_activites.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo_activites.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hamalmar <hamalmar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:09:12 by hamad             #+#    #+#             */
-/*   Updated: 2025/02/09 13:09:22 by hamalmar         ###   ########.fr       */
+/*   Updated: 2025/02/10 01:58:46 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 void	print_status(t_philo philo, int activity)
 {
+    pthread_mutex_lock(&philo.prog->print);
 	if (activity == e_pick_up_fork)
-		printf("%ld %ld has taken a fork\n", get_timestamp_ms(), philo.id);
+		printf("%ld %ld %s", (gtms() - philo.prog->time), philo.id, TF);
 	else if (activity == e_eat)
-		printf("%ld %ld eating\n", get_timestamp_ms(), philo.id);
+		printf("%ld %ld %s", gtms() - philo.prog->time, philo.id, EAT);
 	else if (activity == e_sleep)
-		printf("%ld %ld sleeping\n", get_timestamp_ms(), philo.id);
+		printf("%ld %ld %s", gtms() - philo.prog->time, philo.id, SLP);
 	else if (activity == e_think)
-		printf("%ld %ld thinking\n", get_timestamp_ms(), philo.id);
+		printf("%ld %ld %s", gtms() - philo.prog->time, philo.id, TK);
 	else if (activity == e_dead)
-		printf("%ld %ld died\n", get_timestamp_ms(), philo.id);
+		printf("%ld %ld %s", gtms() - philo.prog->time, philo.id, DEAD);
+    pthread_mutex_unlock(&philo.prog->print);
 }
 
 void	get_forks(t_philo philo)
@@ -34,6 +36,7 @@ void	get_forks(t_philo philo)
 void	eat(t_philo philo)
 {
 	print_status(philo, e_eat);
+
 }
 
 void	think(t_philo philo)
