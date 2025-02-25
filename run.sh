1872d4da9ddf11fb1479e051de404ee9b7d0b158
Hamad <AlmarzooqiH23@gmail.com>
2025-02-25 21:10:14 +0400

    Changed the test.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 !/bin/bash
-make re ; clear ; valgrind --tool=helgrind -s --log-file="out.log" ./philo 5 500 200 200
+make re ; clear ; valgrind --tool=helgrind -s --log-file="out.log" ./philo 150 1200 300 250 5
